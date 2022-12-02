07a5bcd4c6439ae47cd22b6dde176a9d6f2d6ad2
IAmProgrammist <vladislav.pakhomov@bk.com>
2022-12-02 23:48:21 +0300

    Lab8 mid-done, small project development started

--- lab6/0/test.c
+++ lab6/0/test.c
@@ -5,6 +5,7 @@ void test() {
 
 int main() {
     for (unsigned long long i = 0; i < 0xffffffffffffffffull; i++)
+
         registerTestFromFilePath(
                 "C:\\Users\\vladi\\Workspace\\C\\programming-and-algorithmization-basics\\lab6\\tests.json", "tests",
                 "you look", "stupid");
