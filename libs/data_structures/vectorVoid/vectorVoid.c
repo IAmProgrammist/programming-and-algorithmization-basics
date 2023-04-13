cc37a52e54212469a2b68fd0e5cacefe3dd6a6a8
iamprogrammist <vladislav.pakhomov@bk.ru>
2023-04-13 20:45:58 +0300

    refactoring / bug fix

--- libs/data_structures/vectorVoid/vectorVoid.c
+++ libs/data_structures/vectorVoid/vectorVoid.c
@@ -2,20 +2,24 @@
 
 VectorVoid createVectorV(size_t n, size_t baseTypeSize) {
     VectorVoid resultVector = {malloc(baseTypeSize * n), 0, n, baseTypeSize};
+
     if (resultVector.data == NULL) {
         fprintf(stderr, "bad alloc");
         exit(1);
     }
+
     return resultVector;
 }
 
 void reserveV(VectorVoid *v, size_t newCapacity) {
     if (newCapacity == 0) {
         deleteVectorV(v);
+
         *v = (VectorVoid) {NULL, 0, 0, v->baseTypeSize};
     } else {
         *v = (VectorVoid) {realloc(v->data, newCapacity * v->baseTypeSize), min(newCapacity, v->size), newCapacity,
                            v->baseTypeSize};
+
         if (v->data == NULL) {
             fprintf(stderr, "bad alloc");
             exit(1);
